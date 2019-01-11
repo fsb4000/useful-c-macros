0266212cdb39ab8fe28e269010e71f195ec73741
fsb4000 <fsb4000@yandex.ru>
2019-01-12 03:37:05 +0700

    add attribute CONST

--- attributes.h
+++ attributes.h
@@ -6,9 +6,11 @@
 #ifdef __GNUC__
 #define NONNULL(...) __attribute__((nonnull (__VA_ARGS__)))
 #define PURE __attribute__((pure))
+#define CONST __attribute__((const))
 #else
 #define NONNULL(...)
 #define PURE
+#define CONST
 #endif
 
 #endif
