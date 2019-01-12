d79476ad7b5b489aca602ac50add3f4d07ff4ae0
fsb4000 <fsb4000@yandex.ru>
2019-01-12 07:46:09 +0700

    fix CAST_UINT32_TO_SIZE_T for OpenBSD

--- 32bit_support.h
+++ 32bit_support.h
@@ -6,7 +6,7 @@
 #include <limits.h>
 #include <inttypes.h>
 
-#if ((SIZE_MAX) == (UINT32_MAX)) && !defined(__HAIKU__)
+#if ((SIZE_MAX) == (UINT32_MAX)) && !defined(__HAIKU__) && !defined(__OpenBSD__)
 #define CAST_UINT32_TO_SIZE_T
 #define CAST_SIZE_T_TO_UINT32
 #else
